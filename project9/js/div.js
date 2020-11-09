173c535af0777076fb0053449eb814f1fa288580
Tin Tin <tint8140@gmail.com>
2020-11-09 14:13:49 -0500

    jlkafa

--- project9/js/div.js
+++ project9/js/div.js
@@ -1,15 +1,22 @@
-var board = document.createElement('squares');
-var boards= [];
-board.style.width= "20px";
-board.style.height="20px";
-board.style.margin="100px";
-board.style.float= "left";
-
-
-
-function squareFunction() {
-    for(x=0; x>=100;x++) {
+for (var i = 1; i <= 100; i++ ) {
+    var Box = document.createElement("div2");
     
+
+    //generate the RGB format
+    var randomColor = Math.floor(Math.random() * 256);
+    var randomColor2 = Math.floor(Math.random() * 256);
+    var randomColor3 = Math.floor(Math.random() * 256);
+
+    //Box sizes and float to left
+    Box.style.width = "20px";
+    Box.style.height = "20px";
+    Box.style.float = "left";
+
+
+    //RGB Colors
+    Box.style.backgroundColor = "rgb(" + randomColor + ","+randomColor2+","+randomColor3+")";
+
+  document.body.appendChild(Box);
     }
-}
+ 
 
